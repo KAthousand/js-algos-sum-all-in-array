9f59c996db00d96ab4a47737c5d70f48ff66b6c5
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-09-30 22:04:01 -0400

    kyle thousand

--- sumAllInArray.js
+++ sumAllInArray.js
@@ -1,9 +1,16 @@
 //SUM ALL IN ARRAY
 
+let total = 0
 const sumAllInArray = arr => {
 
+  arr.forEach((num) => {
+    if (isNaN(num) === false) {
+      total += Number(num)
+    }
+  })
+  return total
 }
-
+//parseInt
 console.log(sumAllInArray(["-1", "a", 4, "-32", 8, "94"]))
 
 module.exports = {
